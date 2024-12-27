e8ce686c6737ff291c47fe9800edfebd0c60cc1f
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2024-12-27 20:28:18 +0100

    Feat: Add linkedIn logo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container"),  
     tile = document.querySelector(".tile");
 
-for(let i = 0; i < 1439; i++) {
+for(let i = 0; i < 2699; i++) {
   container.appendChild(tile.cloneNode());
 }
