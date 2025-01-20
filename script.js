d37749b28f1008755741718426e49e2ab4a13c97
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-01-20 15:04:47 +0100

    Feat: Add Index Pages

--- script.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector("#container"),  
-    tile = document.querySelector(".tile");
-
-for(let i = 0; i < 2699; i++) {
-  container.appendChild(tile.cloneNode());
-}
